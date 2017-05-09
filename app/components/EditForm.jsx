9544e7c95a42866b4c48428172883ff57a0fe5a5
Роман <roman.sarder@yandex.ru>
2017-05-09 21:24:19 +0300

    Edit form component handle undefined error

--- app/components/EditForm.jsx
+++ app/components/EditForm.jsx
@@ -10,7 +10,7 @@ class EditForm extends Component {
     }
     
     componentWillMount() {
-        let {auth, dispatch} = this.props;
+        let {dispatch, auth} = this.props;
         if (!auth.token) {
             dispatch(actions.setError('You must login.'))
             dispatch(push('/'));
@@ -19,10 +19,10 @@ class EditForm extends Component {
     
     handleSubmit(e) {
         e.preventDefault();
+        let {dispatch, auth, params} = this.props;
         let state = this.refs.state[this.refs.state.selectedIndex].value
         let name = this.refs.name.value;
         let number = this.refs.number.value;
-        let {dispatch, auth, params} = this.props;
         this.props.dispatch(actions.updateItem({
             name,
             state,
@@ -33,37 +33,37 @@ class EditForm extends Component {
 
     }
     render() {
-        // let {isLoading, items, match} = this.props;
-        // let thisItem = items.filter((item) => {
-        //     return item.id === match.params.id
-        // });
-        let renderLoading = () => {
-            // if (isLoading) {
-            //     return 'Loading...'
-            // } else {
-            //     return 'Submit';
-            // }
-            return 'Submit';
-        }
+        let {isLoading, items, params} = this.props;
+        let currentItem = items.filter((item) => {
+            console.log(typeof item.id);
+            return item.id === parseInt(params.id);
+        })[0];
+        // Create object to prevent error 'cannot read prop of undefined'
+        // when user types this component's url directly
+        let formInfo = {
+            name: currentItem ? currentItem.name : '',
+            number: currentItem ? currentItem.number : '',
+            state: currentItem ? currentItem.state : ''
+        };
         return (
             <div className="form-container">
                 <form onSubmit={this.handleSubmit}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="email" placeholder="Name" ref="name" />
+                        <input type="text" name="email" placeholder="Name" ref="name" defaultValue={formInfo.name}/>
                     </div>
                     <div className="field">
                         <label>Number</label>
-                        <input type="text" name="password" placeholder="Number" ref="number" />
+                        <input type="text" name="password" placeholder="Number" ref="number" defaultValue={formInfo.number} />
                     </div>
                     <div className="field">
                         <label>State</label>
-                        <select name="state" ref="state">
+                        <select name="state" ref="state" defaultValue={formInfo.state}>
                             <option value="In transit">In transit</option>
                             <option value="In warehouse">In warehouse</option>
                         </select>
                     </div>
-                    <button className="form-button" type="submit">{renderLoading()}</button>
+                    <button className="form-button" type="submit">Submit</button>
                 </form>
             </div>
         );
